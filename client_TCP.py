36ee9c34c99774da72d1affe49d5a8a3f364f709
parth-1711 <parthirache8@gmail.com>
2024-09-13 00:10:20 +0530

    added multicast functionality

--- client_TCP.py
+++ client_TCP.py
@@ -1,11 +1,15 @@
 import socket 
 import threading
 import os
+import time
 
 nickname = input("Choose a nickname: ")
+password = input("Enter password to join the private network: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('<SERVER IP>', 55555))
+client.connect(('10.2.0.152', 55555)) #replace your server's ip with the ip mentioned here.
+
+BUFSIZE=1024
 
 def receive():
     while True:
@@ -13,9 +17,32 @@ def receive():
             message = client.recv(1024).decode()
             if message == 'NICK':
                 client.send(nickname.encode())
+            elif message == 'PASSWORD':
+                client.send(password.encode())
+            elif message == "Wrong Password":
+                    print("Password is incorrect.")
+                    client.close()
+                    # exit()
+                    break
+            elif message=='Throughput':
+                print('hello')
+                testdata = 'x' * (BUFSIZE-1) + '\n'
+                t1 = time.perf_counter()
+                i = 0
+                while i < 10:
+                    i = i+1
+                    client.send(testdata.encode())
+                    data = client.recv(64).decode()
+                t2 = time.perf_counter()
+               
+                t3 = time.perf_counter()
+                
+                throghput=round(((BUFSIZE+64)*10*0.001) / (t3-t1), 3)
+                print('Throughput is : ',throghput,' Kbps')
             else:
                 print(message)
         except:
+            
             print("An error occured!")
             client.close()
             break
@@ -24,7 +51,7 @@ def write():
     connected=True
     while connected:
         try:
-            msg_type=input("")
+            msg_type=input()
             msg_length=len(msg_type)
             
             send_length=str(msg_length).encode()
@@ -34,15 +61,16 @@ def write():
             client.send(msg_type.encode())
 
             if msg_type.strip().lower()=='broadcast':
+                
                 message = f'{nickname}: {input("")} '
                 msg_length=len(message)
-            # print(msg_length)
                 send_length=str(msg_length).encode()
                 send_length+=b' '*(64-len(send_length))
             
                 client.send(send_length)
             
                 client.send(message.encode())
+                print('Message Sent !')
 
             elif msg_type.strip().lower()=='unicast':
                 
@@ -57,15 +85,49 @@ def write():
                 
                 message = f'{nickname}: {input("")} '
                 msg_length=len(message)
-            # print(msg_length)
                 send_length=str(msg_length).encode()
                 send_length+=b' '*(64-len(send_length))
             
                 client.send(send_length)
             
-                client.send(message.encode())  
-        except e:
-            print(e)
+                client.send(message.encode()) 
+                print('Message Sent !')
+                
+            elif msg_type.strip().lower()=='multicast':
+                
+                group = [] 
+
+                grplen = input("Enter number of people in the group : ")
+
+                for i in range(int(grplen)):
+                    temp = input("Select nickname to include in multicast : ")
+                    group.append(temp)
+
+                grp = " ".join(group)
+                grplen = len(grp)
+            
+                send_length=str(grplen).encode()
+                send_length+=b' '*(64-len(send_length))
+                
+                client.send(send_length)
+                client.send(grp.encode())
+                
+                print("Enter message to multicast :")
+                message = f'{nickname}: {input("")} '
+                msg_length=len(message)
+                
+                send_length=str(msg_length).encode()
+                send_length+=b' '*(64-len(send_length))
+            
+                client.send(send_length)
+            
+                client.send(message.encode()) 
+                print('Message Sent !')
+        except:
+            print("An error occured !")
+            # client.close()
+            connected=False
+            break
         # connected=False
 
 receive_thread = threading.Thread(target=receive)
--- server_TCP.py
+++ server_TCP.py
@@ -1,6 +1,6 @@
 import threading
 import socket
-
+# import sys
 
 host = socket.gethostbyname(socket.gethostname())
 port = 55555
@@ -11,6 +11,8 @@ server.listen()
 
 clients = []
 nicknames = []
+
+PASSWORD = "password"
 
 def broadcast(message):
     # print(f"activve clients {clients}")
@@ -25,14 +27,21 @@ def unicast(nickname,msg):
     client.send(msg)
     print(f"msg sent to {nickname}")
 
+def multicast(message, group):
+    # print(f"active clients {clients}")
+    for client in group:
+        index=nicknames.index(client)
+        clt=clients[index]
+        # clt.send(msg)
+        clt.send(message)
+
 def handle(client):
     while True:
-        try:
-            
+        try:    
             msg_length=int(client.recv(64).decode())
             # print(msg_length)
             msg_type = client.recv(msg_length).decode()
-            # print(msg_type)
+            print(msg_type)
             
             
             if msg_type.strip().upper()=='BROADCAST':
@@ -43,6 +52,7 @@ def handle(client):
                 message=client.recv(msg_length).decode()
                 # print(message)
                 broadcast(message.encode())
+
             elif msg_type.strip().upper()=='UNICAST':
                 msg_length=int(client.recv(64).decode())
                 nickname=client.recv(msg_length).decode()
@@ -50,12 +60,25 @@ def handle(client):
                 msg_length=int(client.recv(64).decode())
                 # print(msg_length)
                 
-                message=client.recv(msg_length).decode()
+                message=client.recv(msg_length).decode() +'(unicasted)'
                 # msg=client.recv(1024)
-                message+='(Unicasted)'
                 unicast(nickname,message.encode())
+
+            elif msg_type.strip().upper()=='MULTICAST':    
+                
+
+                grouplen = client.recv(64).decode()
+                grouplen = int(grouplen)
+                group = client.recv(grouplen).decode()
+                group = group.split(' ')
+
+                msg_length=int(client.recv(64).decode())                
+                message=client.recv(msg_length).decode()
+
+                # msg=client.recv(1024)
+                message+='(Multicast)'
+                multicast(message.encode(), group)
         except:
-            # print(e)
             index = clients.index(client)
             clients.remove(client)
             client.close()
@@ -67,19 +90,122 @@ def handle(client):
 def receive():
     while True:
         client, address = server.accept()
-        print(f"Connected with {str(address)}")
 
         client.send('NICK'.encode())
         nickname = client.recv(1024).decode()
-        nicknames.append(nickname)
-        clients.append(client)
+        client.send('PASSWORD'.encode())
+        password = client.recv(1024).decode()
 
-        print(f"Nickname of the client is {nickname}!")
-        broadcast(f"{nickname} has joined the Network!".encode())
-        client.send('Connected to the server!'.encode())
+        if password != PASSWORD:
+            client.send("Wrong Password".encode())
+            client.close()
+        else:
+            print(f"Connected with {str(address)}")
 
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+            nicknames.append(nickname)
+            clients.append(client)
+
+
+            print(f"Nickname of the client is {nickname}!")
+            broadcast(f"{nickname} has joined the Network!".encode())
+            # client.send('Connected to the server!'.encode())
+            
+            client.send('Throughput'.encode())
+            iter=0
+            BUFSIZE=1024
+            ack=b' '*64
+            while iter<10:
+                data = client.recv(BUFSIZE)
+                    # print(data.decode())
+                client.send(ack)
+                iter+=1
+                # conn.close()
+            #     print ('Done')
+            # print('done2')
+
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.start()
+
+
 
 print('Server is listening...')
 receive()
+
+
+# -------------------------------------
+
+
+
+# def main():
+#     if len(sys.argv) < 2:
+#         usage()
+#     if sys.argv[1] == '-s':
+#         server()
+#     elif sys.argv[1] == '-c':
+#         client()
+#     else:
+#         usage()
+
+
+# def usage():
+#     sys.stdout = sys.stderr
+#     print ('Usage:    (on host_A) throughput -s [port]')
+#     print ('and then: (on host_B) throughput -c count host_A [port]')
+#     sys.exit(2)
+
+
+# def server():
+#     if len(sys.argv) > 2:
+#         port = eval(sys.argv[2])
+#     else:
+#         port = MY_PORT
+#     s = socket(AF_INET, SOCK_STREAM)
+#     s.bind(('', port))
+#     s.listen(1)
+#     print ('Server ready...')
+#     while 1:
+#         conn, (host, remoteport) = s.accept()
+#         while 1:
+#             data = conn.recv(BUFSIZE)
+#             if not data:
+#                 break
+#             del data
+#         conn.send('OK\n')
+#         conn.close()
+#         print ('Done with', host, 'port', remoteport)
+
+
+# def client():
+#     if len(sys.argv) < 4:
+#         usage()
+#     count = int(eval(sys.argv[2]))
+#     host = sys.argv[3]
+#     if len(sys.argv) > 4:
+#         port = eval(sys.argv[4])
+#     else:
+#         port = MY_PORT
+#     testdata = 'x' * (BUFSIZE-1) + '\n'
+#     t1 = time.time()
+#     s = socket(AF_INET, SOCK_STREAM)
+#     t2 = time.time()
+#     s.connect((host, port))
+#     t3 = time.time()
+#     i = 0
+#     while i < count:
+#         i = i+1
+#         s.send(testdata)
+#     s.shutdown(1) # Send EOF
+#     t4 = time.time()
+#     data = s.recv(BUFSIZE)
+#     t5 = time.time()
+#     print (data)
+#     print ('Raw timers:', t1, t2, t3, t4, t5)
+#     print ('Intervals:', t2-t1, t3-t2, t4-t3, t5-t4)
+#     print ('Total:', t5-t1)
+#     print ('Throughput:', round((BUFSIZE*count*0.001) / (t5-t1), 3),)
+#     print ('K/sec.')
+
+
+# main()
+# cd "Final Code"  
+# python client_TCP.py
